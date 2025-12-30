dfba9e44311cd17380b9631141af607ef0f3a4e3
DernaneAkil <dernanedjihad8@gmail.com>
2025-12-30 17:20:13 +0100

    first commit

--- config/settings.py
+++ config/settings.py
@@ -10,4 +10,3 @@ EXAM_DURATION_MIN = 120
 MAX_EXAMS_PER_PROF_PER_DAY = 3
 MAX_EXAMS_PER_STUDENT_PER_DAY = 1
 EXAM_PERIOD_DAYS = 10
-mysql://root:MrclKbTPkcvCxSTUIPCxAfSXkwCZJtaC@turntable.proxy.rlwy.net:31145/edt_examens
