9f9e2bd26583d749429d53b9c6e5e84fe97e63e9
DernaneAkil <dernanedjihad8@gmail.com>
2025-12-30 17:16:38 +0100

    first commit

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     "host": "proxy.rlwy.net",
-    "user": "freedb_akil_dernane",
+    "user": "root",
     "password": "MrclKbTPkcvCxSTUIPCxAfSXkwCZJtaC",
     "database": "edt_examens",
     "port": 31145,
@@ -10,3 +10,4 @@ EXAM_DURATION_MIN = 120
 MAX_EXAMS_PER_PROF_PER_DAY = 3
 MAX_EXAMS_PER_STUDENT_PER_DAY = 1
 EXAM_PERIOD_DAYS = 10
+mysql://root:MrclKbTPkcvCxSTUIPCxAfSXkwCZJtaC@turntable.proxy.rlwy.net:31145/edt_examens
